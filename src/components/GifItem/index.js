d2d8af53e67a826821c6e9b99d6e4c90ba0081dd
Dean Le <dean.le@transwap.com>
2021-04-14 16:23:35 +0700

    implement click to view full image

--- src/components/GifItem/index.js
+++ src/components/GifItem/index.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Card from '@material-ui/core/Card';
 import LinkIcon from '@material-ui/icons/Link';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import CloseIcon from '@material-ui/icons/Close';
 import useStyles from "./styles";
 
 const GifItem = ({ data }) => {
   const classes = useStyles();
+
+  const [selected, setSelected] = useState(false);
+
+  useEffect(() => {
+    document.body.style.overflow = selected ? 'hidden' : 'unset';
+  }, [selected])
 
   const {
     images,
@@ -16,10 +23,19 @@ const GifItem = ({ data }) => {
     user,
   } = data;
 
+  const onToggleSelectImage = () => {
+    setSelected(!selected);
+  }
+
   return (
     <div>
       <Card className={classes.root}>
-        <img className={classes.image} src={images.downsized.url} alt={slug} />
+        <img
+          className={classes.image}
+          src={images.downsized.url}
+          alt={slug}
+          onClick={onToggleSelectImage}
+        />
 
         <div className={classes.actions}>
           <div>
@@ -46,6 +62,14 @@ const GifItem = ({ data }) => {
         <div className={classes.userInfo}>
           <img className={classes.avatar} src={user.avatar_url} alt={user.username} />
           <span className={classes.userName}>{user.display_name}</span>
+        </div>
+      )}
+
+      {selected && (
+        <div className={classes.modalView}>
+          <div className={classes.faded} />
+          <CloseIcon className={classes.closeIcon} onClick={onToggleSelectImage} />
+          <img className={classes.modalImage} src={images.original.url} alt={slug} />
         </div>
       )}
     </div>
--- src/components/GifItem/styles.js
+++ src/components/GifItem/styles.js
@@ -7,6 +7,7 @@ const useStyles = makeStyles((theme) => ({
   image: {
     width: "100%",
     height: 220,
+    cursor: "pointer",
     [theme.breakpoints.down("sm")]: {
       height: 120,
     },
@@ -55,6 +56,36 @@ const useStyles = makeStyles((theme) => ({
   userName: {
     color: "#1976d2",
     fontWeight: "600",
+  },
+  modalView: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    zIndex: 99,
+  },
+  faded: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#000",
+    opacity: 0.9,
+  },
+  closeIcon: {
+    color: "#fff",
+    cursor: "pointer",
+    position: "absolute",
+    top: 20,
+    right: 20,
+    zIndex: 999,
+  },
+  modalImage: {
+    width: "80%",
+    maxWidth: 700,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
   },
 }));
 
